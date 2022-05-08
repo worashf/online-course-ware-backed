4333a91dcfd027ecdaeb44c257337d6967c0bbce
worashf <uppert83@gmail.com>
2022-05-08 21:43:51 +0300

    update blank question entity

--- src/main/java/com/online/course/course_ware/entity/BlankQuestion.java
+++ src/main/java/com/online/course/course_ware/entity/BlankQuestion.java
@@ -4,9 +4,12 @@ package com.online.course.course_ware.entity;
 import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -21,6 +24,45 @@ public class BlankQuestion implements Serializable{
     private String questionName;
      @OneToOne(mappedBy = "blankQuestion")
     private Answer answer; 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name ="question_category_id")
+    private QuestionCategory questionCategory;
+
+    public Long getBlankQuestionId() {
+        return blankQuestionId;
+    }
+
+    public void setBlankQuestionId(Long blankQuestionId) {
+        this.blankQuestionId = blankQuestionId;
+    }
+
+    public String getQuestionName() {
+        return questionName;
+    }
+
+    public void setQuestionName(String questionName) {
+        this.questionName = questionName;
+    }
+
+    public Answer getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(Answer answer) {
+        this.answer = answer;
+    }
+
+    public QuestionCategory getQuestionCategory() {
+        return questionCategory;
+    }
+
+    public void setQuestionCategory(QuestionCategory questionCategory) {
+        this.questionCategory = questionCategory;
+    }
+
+
+     
+     
     
     
     
